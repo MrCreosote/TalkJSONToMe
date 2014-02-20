99fd470378b1c7802e68248c6b4d91704bf4c221
Gavin Price <gaprice@lbl.gov>
2014-02-20 11:49:13 -0800

    Test perl inf/nan output w/ python

--- src/pyJSON.py
+++ src/pyJSON.py
@@ -5,5 +5,12 @@ d = {'Not a number': float('nan'), 'To infinity etc': float('inf')}
 print "Python testing " + str(d) + ":"
 print "json.dumps():"
 print json.dumps(d)
-print "json.loads()"
+print "json.loads():"
 print json.loads('{"Not a number": NaN, "To infinity etc": Infinity}')
+perlstr = '{"Not a number": nan, "To infinity etc": inf}'
+print 'testing ' + perlstr
+print "json.loads():"
+try:
+    print json.loads(perlstr)
+except ValueError as err:
+    print err
